391563336b2522f3f5fef3b31fd1b0bdd26fe3ff
Ben Lamm <benlamm25@gmail.com>
2024-02-21 19:05:42 -0500

    Initial commit

---
+++ test/ftp2_s3.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Ftp2S3 = require('../lib/ftp2_s3-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ftp2_s3-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Ftp2S3.Ftp2S3Stack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
